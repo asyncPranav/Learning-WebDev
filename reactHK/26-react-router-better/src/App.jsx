5f79f53c4e1d1e6863e047f28fc24a2ace821906
redxcyber <mrdark9628@gmail.com>
2025-08-10 20:52:42 +0530

    add react router better

---
+++ reactHK/26-react-router-better/src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+// Layout with Navbar
+function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><Home /></Layout>
+  },
+  {
+    path: "/about",
+    element: <Layout><Projects /></Layout>
+  },
+  {
+    path: "/contact",
+    element:<Layout><Contact /></Layout>
+  },
+  {
+    path: "/projects",
+    element: <Layout><About /></Layout>
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App
