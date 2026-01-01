c3703b1c09a22c4497e2d4b982cb60af5d49876b
asyncPranav <asyncpranav@gmail.com>
2026-01-01 19:36:31 +0530

    add ep-03

---
+++ backend/season-01/episode-01/01.js
@@ -0,0 +1,17 @@
+let name = "Pranav SIngh Chandel";
+
+let a = 23;
+let b = 20;
+
+console.log(a + b);
+console.log(name);
+
+console.log(global);
+
+console.log(this); // Empty object
+
+console.log(globalThis);
+console.log(globalThis === global);
+
+
+
