b3c6fc1a3f6697a2491a23fa651572513b6c17f7
asyncPranav <asyncpranav@gmail.com>
2026-01-28 19:48:12 +0530

    add 10-use-state

---
+++ frontend/10-use-state/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Counter count={count} setCount={setCount} />
+    </div>
+  );
+};
+
+const Counter = ({ count, setCount }) => {
+  // function increment() {
+  //   setCount(count + 1);
+  // }
+
+  // const increment = () => {
+  //   return setCount(count + 1)
+  // }
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}><b>Increment</b></button>
+      <button onClick={decrement}><b>Decrement</b></button>
+      <button onClick={reset}><b>Reset</b></button>
+    </div>
+  );
+};
+
+export default App;
