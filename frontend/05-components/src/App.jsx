9115e9e32e2c840dcf2c03b69633100578e4f325
asyncPranav <asyncpranav@gmail.com>
2026-01-28 16:06:06 +0530

    add 05-components

---
+++ frontend/05-components/src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./Hello.jsx";
+import Bye from "./Bye.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/05-components/src/Hello.jsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello from Hello.jsx component</h1>
+    </div>
+  );
+};
+
+export default Hello;
