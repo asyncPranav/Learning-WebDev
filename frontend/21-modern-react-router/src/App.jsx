e8a7891e3499c57dd8eb811805bde57b867dcfd7
asyncPranav <asyncpranav@gmail.com>
2026-02-03 09:37:47 +0530

    add 21-modern-react-router

---
+++ frontend/21-modern-react-router/src/App.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Navbar from "./Navbar"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home/>
+      </div>
+    )
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <Navbar />
+        <About />
+      </div>
+    )
+  },
+  {
+    path: "/contact",
+    element: (
+      <div>
+        <Navbar />
+        <Contact/>
+      </div>
+    )
+  }
+])
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
