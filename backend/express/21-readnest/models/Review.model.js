86d2e78a1a8a964dd6fbfa0681510bba5ac1060c
asyncPranav <asyncpranav@gmail.com>
2026-05-16 17:45:02 +0530

    add review model

---
+++ backend/express/21-readnest/models/Review.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
