2b9bca8eee8dce9c593a91c2d5fa44b16d22fe84
asyncPranav <asyncpranav@gmail.com>
2026-06-03 17:07:59 +0530

    add 404 route and err handling middleware

--- backend/express/23-api-development/index.js
+++ backend/express/23-api-development/index.js
@@ -4,6 +4,8 @@ const app = express();
 const connectDB = require("./config/db");
 const studentsRouter = require("./routes/students.route");
 
+const multer = require("multer"); // for error handling in the error handling middleware for multer errors
+
 const PORT = process.env.PORT;
 
 // Connect to MongoDB
@@ -17,6 +19,25 @@ app.use(express.json());
 
 // use students router
 app.use("/api/students", studentsRouter);
+
+// 404 handler for undefined routes
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    // Handle Multer-specific errors
+    return res.status(400).json({ multer_error: err.message });
+  } else if (err) {
+    // Handle general errors
+    return res.status(500).json({ server_error: err.message });
+  }
+  next();
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
